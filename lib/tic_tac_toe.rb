1c00151ae4a116c17a7ae3e39881f5c695bd09e4
Jason Hang <jasonxhang@gmail.com>
2018-06-28 02:06:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,9 +93,9 @@ end
 def play(board)
 until over?(board)
     turn(board)
-  if won?(board) && current_player(board) == "X"
+  if won?(board) && winner(board) == "X"
     puts "Congratulations X!"
-    elsif won?(board) && current_player(board) == "O"
+    elsif won?(board) && winner(board) == "O"
     puts "Congratulations O!"
     elsif draw?(board)
     puts "Cat's game!"
